2a5b54ff7892531f627aa3e58d65646e5606128a
Linkon <flinkon9@gmail.com>
2025-08-23 01:06:06 +0600

    price

--- src/pages/student/CourseDeatials.jsx
+++ src/pages/student/CourseDeatials.jsx
@@ -3,11 +3,13 @@ import { useParams } from 'react-router-dom';
 import { AppContext } from '../../context/AppContext';
 import Loading from '../../components/student/Loading';
 import { assets } from '../../assets/assets';
+import humanizeDuration from 'humanize-duration';
 
 const CourseDeatials = () => {
 
     const {id} = useParams()
     const [courseData , setCourseData] = useState(null)
+    const [openSection ,setOpenSection] = useState({})
 
     const {allCourses,ratingCalculator,calculateNoOfLectures,calculateCourseDuration,calculateChapterTime} = useContext(AppContext)
 
@@ -24,6 +26,15 @@ const CourseDeatials = () => {
 
         fetchCourseData();
     },[])
+
+    const toggleSection = (index) => {
+        setOpenSection((prev)=>(
+            {...prev,
+                [index]: !prev[index],
+
+            }
+        ))
+    }
 
 
     return courseData ? (
@@ -61,18 +72,49 @@ const CourseDeatials = () => {
                                         {
                                             courseData.courseContent.map((chapter,index)=>(
 
-                                                <div key={index}>
-                                                    <div>
-                                                        <div>
-                                                            <img src={assets.down_arrow_icon} alt="" />
-                                                            <p>{chapter.chapterTitle}</p>
+                                                <div key={index} className='border border-gray-300 bg-white mb-2 rounded'>
+                                                    <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none' onClick={()=> toggleSection(index) }>
+                                                        <div className='flex items-center gap-2'>
+                                                            <img className={`transform transition-transform ${openSection[index] ? 'rotate-180': '' }`} src={assets.down_arrow_icon} alt="" />
+                                                            <p className='font-medium md:text-base text-sm'>{chapter.chapterTitle}</p>
                                                         </div>
+                                                        <p className='text-sm md:text-default'>{chapter.chapterContent.length} lectures -
+                                                            {calculateChapterTime(chapter)}
+                                                        </p>
                                                     </div>
+
+                                                    <div className={`overflow-hidden transition-all duration-300 ${openSection[index] ? 'max-h-96' : 'max-h-0'}`}>
+
+                                                    <ul className='list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-green-300'>
+                                                        {chapter.chapterContent.map((lecture,idx)=>(
+                                                            <li key={idx} className='flex items-start gap-2 py-1'>
+                                                                <img src={assets.play_icon} alt="play_icon" className='w-4 h-4 mt-1' />
+                                                                <div className='flex items-center justify-between w-full text-gray-800 text-xs md:text-default'>
+                                                                    <p>
+                                                                        {lecture.lectureTitle}
+                                                                    </p>
+                                                                    <div className='flex gap-2'>
+                                                                        {lecture.isPreviewFree && <p className='text-blue-500 cursor-pointer'>Preview</p>}
+                                                                        <p>{humanizeDuration(lecture.lectureDuration * 60 * 1000, {units: ['h','m']})}</p>
+                                                                    </div>
+                                                                </div>
+                                                            </li>
+                                                        ))}
+                                                    </ul>
+                                                    
+                                                    </div>
+
+
                                                 </div>
 
                                             ))
                                         }
 
+                                    </div>
+
+                                    <div className='py-20 text-sm md:text-default'>
+                                        <h3 className='text-xl font-semibold text-gray-800'>Course Description</h3>
+                                         <p className='pt-4 rich-text' dangerouslySetInnerHTML={{__html: courseData.courseDescription}}></p>
                                     </div>
 
                                 </div>
@@ -82,8 +124,17 @@ const CourseDeatials = () => {
 
             </div>
 
-           <div>
+           <div className='max-w-course-card z-10 shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[450px]'>
+                <img src={courseData.courseThumbnail} alt="" />
 
+                <div className='p-5'>
+
+                    <div className='flex items-center gap-2'>
+                        <img className='w-3.5' src={assets.time_clock_icon} alt="time_clock_icon" />
+                        <p className='text-red-500'><span className='font-medium'>5 Days</span> left at this price!</p>
+                    </div>
+
+                </div>
             </div>
 
 
